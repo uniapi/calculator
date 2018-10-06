7cd1224b7b94b33af9029aad89656b721c68c30c
Ali Muhammed <uniapi@outlook.com>
2018-10-06 14:15:13 +0300

    Updated [package.json, LICENSE] - updated the package file and the license

--- src/index.js
+++ src/index.js
@@ -1,26 +1,85 @@
+/*************************************************************************
+                          Written by Ali Muhammed
+                            uniapi@outlook.com
+                              October 5, 2018
+*************************************************************************/
+
+const _oparg = new WeakMap();
+const _setop = Symbol('setop');
+const _remop = Symbol('remop');
+const _compute = Symbol('compute');
+
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
+  [_compute](arga, op, argb) {
+    switch (op) {
+    case '**': return arga ** argb;
+    case '*': return arga * argb;
+    case '/': return arga / argb;
+    case '+': return arga + argb;
+    case '-': return arga - argb;
+    default: return 0;
+    }
+  }
+  [_setop](op, arg) {
+    const oparg = _oparg.get(this);
+    oparg.ops.push(op);
+    oparg.args.push(arg);
+    _oparg.set(this, oparg);
+    return this;
+  }
+  [_remop](inx) {
+    const oparg = _oparg.get(this);
+    oparg.ops.splice(inx, 1);
+    oparg.args.splice(inx+1, 1);
+    _oparg.set(this, oparg);
   }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
+  constructor(ininum) {
+    _oparg.set(this, { args: [ininum], ops: [] });
   }
 
-  multiply(number) {
-    // your implementation
+  add(num) {
+    return this[_setop]('+', num);
   }
-
-  devide(number) {
-    // your implementation
+  subtract(num) {
+    return this[_setop]('-', num);
   }
-
-  pow(number) {
-    // your implementation
+  multiply(num) {
+    return this[_setop]('*', num);
+  }
+  devide(num) {
+    return this[_setop]('/', num);
+  }
+  pow(num) {
+    return this[_setop]('**', num);
+  }
+  valueOf() {
+    const oparg = _oparg.get(this);
+    const opset = [["**"],["*","/"],["+","-"]];
+    const args = oparg.args;
+    const ops = oparg.ops;
+    for (const set of opset) {
+      if (!ops.length)
+        break;
+      for (let i=0; i < ops.length; i++) {
+        for (let j=0,len=set.length; j < len; ) {
+          const op = set[j];
+          if (ops[i] == op) {
+            if (op == "**" && args[i+1] == 1) {
+              if (i+1 < ops.length && op == ops[i+1]) {
+                this[_remop](i+1);
+                continue;
+              }
+            }
+            args[i] = this[_compute](args[i], op, args[i+1]);
+            this[_remop](i--);
+            break;
+          }
+          j++;
+        }
+      }
+    }
+    return args[0];
   }
 }
 
